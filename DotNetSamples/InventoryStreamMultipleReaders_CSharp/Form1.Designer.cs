5acc365cf0af26e6b6fb8a2342f7e028bf77947a
ekkilm <ekkilm.ts@gmail.com>
2024-10-04 08:33:19 +0300

    Bigger font in listview

--- DotNetSamples/InventoryStreamMultipleReaders_CSharp/Form1.Designer.cs
+++ DotNetSamples/InventoryStreamMultipleReaders_CSharp/Form1.Designer.cs
@@ -34,8 +34,9 @@
             // listBox1
             // 
             listBox1.Dock = DockStyle.Fill;
+            listBox1.Font = new Font("Segoe UI", 11F);
             listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
+            listBox1.ItemHeight = 20;
             listBox1.Location = new Point(0, 0);
             listBox1.Name = "listBox1";
             listBox1.Size = new Size(800, 450);
